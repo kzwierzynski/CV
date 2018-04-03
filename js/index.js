1cb1d47c60d9068208bcaf1a29a29d6b4bfc9c38
KZW <k.a.zwierzynski@gmail.com>
2018-04-03 23:31:55 +0200

    non dropping down contact info bug fixed, scrolling script changed

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
     // Smooth Scrolling
     $('a[href*="#"]:not([href="#"])').click(function() {
+      if (this.hash == '#contactInfo') return true; //if drop down button -> do nothing
       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-        var target = $(this.hash);
+        let target = $(this.hash);
         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
         if (target.length) {
           $('html, body').animate({
