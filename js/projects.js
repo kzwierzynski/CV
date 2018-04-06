46c45114c7479f2b56f20c3630acd573f28a3ed5
KZW <k.a.zwierzynski@gmail.com>
2018-04-06 17:55:49 +0200

    redone all styling, unified, simplified, better, modifications wrt. mobile version

--- js/projects.js
+++ js/projects.js
@@ -5,6 +5,10 @@ let bottomLayer = wrapper.querySelector('.bottom');     //added
 let bottomWrap = bottomLayer.querySelector('.content-wrap') //added
 let handle = wrapper.querySelector('.handle');
 let bodie = document.body;
+let img = wrapper.querySelector('img');
+
+let sc1 = document.querySelector('.showcase1');
+let sc2 = document.querySelector('.showcase2');
 //initializing
 let skew = 500+1;  // because of the margin
 let delta = 0;
@@ -15,13 +19,19 @@ function setWidth(){
   topLayer.style.width= wrapper.offsetWidth + 'px';
   bottomWrap.style.width= wrapper.offsetWidth + 'px';
   topWrap.style.width= wrapper.offsetWidth + 'px';
-  // console.log(bodyWidth);
+  wrapper.style.height = img.height * 1.8 + 'px';
+  sc2.style.height = sc1.offsetHeight + 'px';
+  
+  console.dir(sc1.offsetHeight);
 }
+
+
 
 document.addEventListener('DOMContentLoaded', function(){
   // added coz not using vw anymore -> in container
   setWidth();
   topLayer.style.width= bodie.offsetWidth/2 -wrapper.offsetLeft + skew + 'px';    
+  wrapper.style.height = img.height * 1.6 + 'px'; // needed at initialization to set size of jumbotron
   
   wrapper.addEventListener('mousemove', function(e){
     delta = (e.clientX - bodie.offsetWidth / 2) *0.3;  //na srodku=0, im dalej od środka tym bardziej popycha "belkę" oddzielającą warstwy
