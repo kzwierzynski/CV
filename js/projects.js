ab63aae6b231f86bd05042205681d4c65a28efe6
KZW <k.a.zwierzynski@gmail.com>
2018-04-11 15:03:40 +0200

    going back with initialization, changed mouse factor for jumbotron skew line

--- js/projects.js
+++ js/projects.js
@@ -1,5 +1,14 @@
-let wrapper, topLayer, topWrap, bottomLayer, bottomWrap, handle, bodie, img, sc1, sc2;
+let wrapper = document.getElementById('wrapper');
+let topLayer = wrapper.querySelector('.top');
+let topWrap = topLayer.querySelector('.content-wrap');  //added
+let bottomLayer = wrapper.querySelector('.bottom');     //added
+let bottomWrap = bottomLayer.querySelector('.content-wrap') //added
+let handle = wrapper.querySelector('.handle');
+let bodie = document.body;
+let img = wrapper.querySelector('img');
 
+let sc1 = document.querySelector('.showcase1');
+let sc2 = document.querySelector('.showcase2');
 //initializing
 let skew = 500+1;  // because of the margin
 let delta = 0;
@@ -19,26 +28,13 @@ function setWidth(){
 
 
 document.addEventListener('DOMContentLoaded', function(){
-  
-wrapper = document.getElementById('wrapper');
-topLayer = wrapper.querySelector('.top');
-topWrap = topLayer.querySelector('.content-wrap');  //added
-bottomLayer = wrapper.querySelector('.bottom');     //added
-bottomWrap = bottomLayer.querySelector('.content-wrap') //added
-handle = wrapper.querySelector('.handle');
-bodie = document.body;
-img = wrapper.querySelector('img');
-
-sc1 = document.querySelector('.showcase1');
-sc2 = document.querySelector('.showcase2');
   // added coz not using vw anymore -> in container
   setWidth();
   topLayer.style.width= bodie.offsetWidth/2 -wrapper.offsetLeft + skew + 'px';    
   // wrapper.style.height = img.height * 1.8 + 'px'; // needed at initialization to set size of jumbotron
-  // sc2.style.height = sc1.offsetHeight + 'px';
   
   wrapper.addEventListener('mousemove', function(e){
-    delta = (e.clientX - bodie.offsetWidth / 2) *0.35;  //na srodku=0, im dalej od środka tym bardziej popycha "belkę" oddzielającą warstwy
+    delta = (e.clientX - bodie.offsetWidth / 2) *0.4;  //na srodku=0, im dalej od środka tym bardziej popycha "belkę" oddzielającą warstwy
       // console.log(e.clientX-wrapper.offsetLeft, delta)
     handle.style.left = e.clientX -wrapper.offsetLeft + delta + 'px';
     //-wrapper.offsetLeft to center the line (offset from container)
