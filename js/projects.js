ae343b03aa1d25f8d0c16daeec57aa9207b18aaf
KZW <k.a.zwierzynski@gmail.com>
2018-04-11 19:04:15 +0200

    projects.html init on clear cache solved, added setInterval + setTimer in projects.js

--- js/projects.js
+++ js/projects.js
@@ -9,52 +9,47 @@ let img = wrapper.querySelector('img');
 
 let sc1 = document.querySelector('.showcase1');
 let sc2 = document.querySelector('.showcase2');
-//initializing
+
 let skew = 500+1;  // because of the margin
 let delta = 0;
-
-console.log("after init: ", wrapper.offsetWidth, img.height, sc1.offsetHeight, sc2.offsetHeight);
 
 
 //setting width of the layers and their content to the width of wrapper/container
 function setWidth(){  
-  console.log("at the beginning of setWidth: ", wrapper.offsetWidth, img.height, sc1.offsetHeight, sc2.offsetHeight);
   bottomLayer.style.width= wrapper.offsetWidth + 'px';
   topLayer.style.width= wrapper.offsetWidth + 'px';
   bottomWrap.style.width= wrapper.offsetWidth + 'px';
   topWrap.style.width= wrapper.offsetWidth + 'px';
   wrapper.style.height = img.height * 1.8 + 'px';
   sc2.style.height = sc1.offsetHeight + 'px';
-  console.log("at the end of setWidth: ", wrapper.offsetWidth, img.height, wrapper.style.height, sc1.offsetHeight, sc2.offsetHeight);
-  // console.dir(sc1.offsetHeight);
 }
-let i=0;
-function checkResize(){ // when delays in loading page, after cache 
-    if (sc1.offsetHeight != sc2.offsetHeight) {
-      console.log(sc1.offsetHeight, sc2.offsetHeight);
-      sc2.style.height = sc1.offsetHeight + 'px';
-      console.log(sc1.style.height, sc2.style.height);
 
+function checkResize(){ // assure proper sizing of showcase2 and jumbotron
+    if (sc1.offsetHeight != sc2.offsetHeight) {
+      sc2.style.height = sc1.offsetHeight + 'px';
+    }
+    if(wrapper.offsetHeight == 0){
+      wrapper.style.height = img.height * 1.8 + 'px';
     }
 }
 
+
 document.addEventListener('DOMContentLoaded', function(){
-  // added coz not using vw anymore -> in container
+  // added setWidth(), coz not set using vw anymore -> here it's in container
   setWidth();
-  console.log("after setWidth: ", wrapper.offsetWidth, img.height, sc1.offsetHeight, sc2.offsetHeight);
   topLayer.style.width= bodie.offsetWidth/2 -wrapper.offsetLeft + skew + 'px';    
-  // wrapper.style.height = img.height * 1.8 + 'px'; // needed at initialization to set size of jumbotron
 
-  let tResize = setInterval(checkResize, 1000);
-
+  let tResize = setInterval(checkResize, 500);  //when delays in loading page, and page not yet in cache
+  let tResizeOff = setTimeout( () => {
+      clearInterval(tResize)
+    }, 5000);
 
   wrapper.addEventListener('mousemove', function(e){
     delta = (e.clientX - bodie.offsetWidth / 2) *0.4;  //na srodku=0, im dalej od środka tym bardziej popycha "belkę" oddzielającą warstwy
       // console.log(e.clientX-wrapper.offsetLeft, delta)
     handle.style.left = e.clientX -wrapper.offsetLeft + delta + 'px';
-    //-wrapper.offsetLeft to center the line (offset from container)
+    //"- wrapper.offsetLeft" to center the line (offset from container)
     topLayer.style.width= e.clientX -wrapper.offsetLeft + skew + delta + 'px';
-    console.log("on mouse move: ", wrapper.offsetWidth, img.height, sc1.offsetHeight, sc2.offsetHeight);
   });
 
 });
